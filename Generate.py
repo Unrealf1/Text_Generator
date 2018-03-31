26a2d2d126bbc0389173c5899361fd6ce3e22fa0
Fyodor <36414426+Unrealf1@users.noreply.github.com>
2018-04-01 04:17:07 +0500

    The last stable version

---
+++ Generate.py
@@ -0,0 +1,54 @@
+import sqlite3
+import argparse
+import random
+
+def weighted_choice(choices):
+   total = sum(w for c, w in choices)
+   r = random.uniform(0, total)
+   upto = 0
+   for c, w in choices:
+      if upto + w >= r:
+         return c
+      upto += w
+   return None
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--model", action="store",
+                    help="model file path", required="true")
+parser.add_argument("-s", "--seed", action="store",
+                    help="initial word")
+parser.add_argument("-l", "--length", action="store",
+                    help="lengs of text in words")
+parser.add_argument("-o", "--output", action="store",
+                    help="output file path")
+#args = parser.parse_args(input().split())
+param = open("gen_param.txt")
+args = parser.parse_args(param.read().split())
+param.close()
+try:
+    file = open(args.model)
+except IOError as e:
+    file.close()
+    print("Error: Can't open model!")
+else:
+    conn = sqlite3.connect(args.model)
+    cursor = conn.cursor()
+
+cur_word = args.seed
+counter = int(args.length)
+while counter > 1:
+    if cur_word == None:
+            sql = "SELECT COUNT(first) FROM dictionary "
+            cnt = cursor.execute(sql).fetchall()
+            rnd = int(random.uniform(0, cnt[0][0]-1))
+            sql = "SELECT first FROM dictionary"
+            cur_word = (cursor.execute(sql).fetchall())
+            cur_word = cur_word[rnd % len(cur_word)][0]
+            print('\n', end=' ')
+    counter -= 1
+    print(cur_word, end=' ')
+    sql = "SELECT second, amount FROM dictionary WHERE first = ?"
+    cursor.execute(sql, [(cur_word)])
+    cur_word = weighted_choice(cursor.fetchall())
+print()
+input()
---
+++ Train.py
@@ -0,0 +1,86 @@
+import sqlite3
+import re
+import os
+import argparse
+import sys
+
+def insert_pair(first, second, data_base_cursor):
+    sql = "SELECT COUNT(*) FROM dictionary WHERE first = ? AND second = ?"
+    cnt = data_base_cursor.execute(sql, (first, second)).fetchall()
+    if cnt[0][0] == 0:
+        data_base_cursor.execute("INSERT INTO dictionary(first, second, amount) VALUES(?, ?, 1)", (first, second))
+    else:
+        data_base_cursor.execute("UPDATE dictionary SET amount = amount + 1 WHERE first = ? AND second = ?", (first, second))
+
+def train(model_path, input_paths, is_lower):
+    try:
+        file = open(model_path)
+    except IOError as e:
+        pass
+    else:
+        with file:
+           file.close()
+           os.remove(model_path)
+
+    conn = sqlite3.connect(model_path)
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE dictionary
+                     (first text, second text, amount integer)""")
+    if input_paths == sys.stdin:
+        input_paths = [sys.stdin]
+    for cur in input_paths:
+        if cur == sys.stdin:
+            input_file = cur
+        else:
+            input_file = open(cur, "r")
+        commit_counter = 0
+        print_counter = 0
+        prev = '-1'
+        for line in input_file:
+            commit_counter += len(line)	
+            if is_lower:
+                line = line.lower()
+            line = re.sub('\d', ' ', line)
+            line = re.sub('_', ' ', line)
+            line = re.findall(r"[\w']+", line)
+            for word in line:
+                if prev == '-1':
+                    prev = word
+                else:
+                    insert_pair(prev, word, cursor)
+                    prev = word
+            if commit_counter >= 20000:
+                print_counter += 1
+                commit_counter = 0
+                conn.commit()
+                if print_counter == 10:
+                    print_counter = 0
+                    print(cur, ' work in progress...')
+        conn.commit()
+        if cur != sys.stdin:
+            print("Trained " + cur)
+
+    print("Train successful")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--model", action="store",
+                    help="model file path", required="true")
+parser.add_argument("-i", "--input_dir", action="store",
+                    help="input directory path")
+parser.add_argument("--lc", action="store_true",
+                    help="all text concidered lower case")
+param = open("train_param.txt")
+args = parser.parse_args(param.read().split())
+param.close()
+#args = parser.parse_args(input().split())
+if args.input_dir != None:
+    files = os.listdir(args.input_dir)
+    i = 0
+    while i < len(files):
+        files[i] = args.input_dir + '/' + files[i]
+        i += 1
+    txt = filter(lambda x: x.endswith('.txt'), files)
+    train(args.model, txt, args.lc)
+else:
+    train(args.model, sys.stdin, args.lc)
+
