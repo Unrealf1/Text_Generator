ec5cd8a668110b649abc97f2e92809df2ccdf8ff
Fyodor Bukreev <Fyodor.bukreev@yandex.ru>
2018-04-14 21:23:16 +0300

    Fixed issues: 8, 9, 11, 14, 15

--- Generate.py
+++ Generate.py
@@ -1,52 +1,49 @@
 import argparse
-import random
 import numpy
 import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--model", action="store",
-                    help="model file path", required="true")
-parser.add_argument("-s", "--seed", action="store",
-                    help="initial word")
-parser.add_argument("-l", "--length", action="store",
-                    help="lengs of text in words")
-parser.add_argument("-o", "--output", action="store",
-                    help="output file path")
-					
-args = parser.parse_args(input().split())
-    
-if not os.path.exists(args.model) or os.path.isfile(args.model):
-    print("Error: Can't open model!")       
 
-cur_word = args.seed
-counter = int(args.length)
-while counter > 1:
-    if cur_word is None:
-            word_list = os.listdir(args.model)
-            cur_word = numpy.random.choice(word_list)
-            cur_word = cur_word[0:-2]
-            print()
-    counter -= 1
-    print(cur_word, end=' ')
-    word_file_name = os.path.join(args.model, cur_word) + ".w"
-    if os.path.exists(word_file_name) and os.path.isfile(word_file_name):
-        word_file = open(word_file_name, 'r')
-        sm = 0
-        poss_word = list()
-        possibility = list()
-        for I in word_file:
-            tmp = I.split()
-            poss_word.append(tmp[0])
-            possibility.append(tmp[1])
-            sm += int(tmp[1])
-        
-        for i in range (len(possibility)):
-            possibility[i] = int(possibility[i]) / sm
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", action="store",
+                        help="model file path", required=True)
+    parser.add_argument("-s", "--seed", action="store",
+                        help="initial word")
+    parser.add_argument("-l", "--length", action="store",
+                        help="lengs of text in words")
+    parser.add_argument("-o", "--output", action="store",
+                        help="output file path")
 
-        cur_word = numpy.random.choice(poss_word, p=possibility)
-            
-    else:
-        cur_word = None
-    
-print()
+    args = parser.parse_args(input().split())
 
+    if not os.path.exists(args.model) or os.path.isfile(args.model):
+        print("Error: Can't open model!")
+
+    cur_word = args.seed
+    counter = int(args.length)
+    while counter > 1:
+        if cur_word is None:
+                word_list = os.listdir(args.model)
+                cur_word = numpy.random.choice(word_list)
+                cur_word = cur_word[0:-2]
+                print()
+        counter -= 1
+        print(cur_word, end=' ')
+        word_file_name = os.path.join(args.model, cur_word) + ".w"
+        if os.path.exists(word_file_name) and os.path.isfile(word_file_name):
+            word_file = open(word_file_name, 'r')
+            sm = 0
+            poss_word = list()
+            possibility = list()
+            for I in word_file:
+                tmp = I.split()
+                poss_word.append(tmp[0])
+                possibility.append(tmp[1])
+                sm += int(tmp[1])
+
+            for i in range(len(possibility)):
+                possibility[i] = int(possibility[i]) / sm
+
+            cur_word = numpy.random.choice(poss_word, p=possibility)
+        else:
+            cur_word = None
--- Train.py
+++ Train.py
@@ -4,66 +4,80 @@ import argparse
 import sys
 import shutil
 
-MAX_INPUT_SIZE = 50000
+# COMMIT_COUNTER stands for amount of symbols we are ready to store in RAM
+COMMIT_TRIGGER = 100000
+# PRINT_TRIGGER stands for amount commits during one file to print, tht program is still working
+PRINT_TRIGGER = 5
 
-def insert_pair(first, second, cursor):
+
+# This function inserts one pair of words(first, second) into the current data correctly
+def insert_pair(first, second, current_data):
     if first == "aux" or second == "aux":
         return
-    if (first, second) in cursor:
-        cursor[(first, second)] += 1
+    if (first, second) in current_data:
+        current_data[(first, second)] += 1
     else:
-        cursor[(first, second)] = 1
+        current_data[(first, second)] = 1
 
-        
-def insert_pair_compress(second, value, cursor):
-    if (second) in cursor:
-        cursor[(second)] += int(value)
+
+# This function inserts word(second because it is going after the word
+# specified in filename) and its amount into current_data
+def insert_pair_compress(second, value, current_data):
+    if second in current_data:
+        current_data[second] += int(value)
     else:
-        cursor[(second)] = int(value)
+        current_data[second] = int(value)
 
-def commit(cursor, model_path):
-    cursor_keys = sorted(cursor.keys())
+
+# This function commits new word pairs from current_data to model on the disc
+def commit(current_data, model_path):
+    current_data_keys = sorted(current_data.keys())
     cur_file = None
     file = None
-    for I in cursor_keys:        
+    for I in current_data_keys:        
         if not cur_file == I[0]:
             cur_file = I[0]
-            if not file is None:
+            if file is not None:
                 file.close()
             file = open(os.path.join(model_path, I[0] + ".w"), 'a')
-        file.write(I[1] + ' ' + str(cursor[I]) + '\n')
-    cursor = dict()
+        file.write(I[1] + ' ' + str(current_data[I]) + '\n')
 
 
-def compress_commit(cursor, file_path):
+# This is the analog of commit, but it's faster, because it works only with one file
+def compress_commit(current_data, file_path):
     file = open(file_path, 'w')
-    for I in cursor:
-        file.write(I + ' ' + str(cursor[I]) + '\n')
-    cursor = dict()
-        
+    for I in current_data:
+        file.write(I + ' ' + str(current_data[I]) + '\n')
+    file.close()
+
+
+# This function compresses data: after Train() there are many pairs stated in
+# same file several times. So compress() unites them in one
 def compress(model_path):
     fl_list = os.listdir(model_path)
     for fl_name in fl_list:
         fl = open(os.path.join(model_path, fl_name))
-        cursor = dict()
+        current_data = dict()
         for line in fl:
             line = line.split()
-            insert_pair_compress(line[0], line[1], cursor)
-        compress_commit(cursor, os.path.join(model_path, fl_name))
-            
+            insert_pair_compress(line[0], line[1], current_data)
+        compress_commit(current_data, os.path.join(model_path, fl_name))
+        fl.close()
+        
 
+
+# This is the main function of the file. It reads all files and creates model
 def train(model_path, input_paths, is_lower):
     if os.path.exists(model_path):
+        print("Deleting old model...")
         if os.path.isfile(model_path):
             os.remove(model_path)
 
         elif os.path.isdir(model_path):
-            shutil.rmtree(model_path)        
-    else:
-        pass
+            shutil.rmtree(model_path)
 
-    os.mkdir(model_path)   
-    cursor = dict()
+    os.mkdir(model_path)
+    current_data = dict()
 
     if input_paths == sys.stdin:
         input_paths = [sys.stdin]
@@ -86,18 +100,19 @@ def train(model_path, input_paths, is_lower):
                 if prev is None:
                     prev = word
                 else:
-                    insert_pair(prev, word, cursor)
+                    insert_pair(prev, word, current_data)
                     prev = word
-            if commit_counter >= MAX_INPUT_SIZE:
+            if commit_counter >= COMMIT_TRIGGER:
                 print_counter += 1
                 commit_counter = 0
-                commit(cursor, model_path)
-                cursor = dict()
-                if print_counter == 10:
+                commit(current_data, model_path)
+                current_data = dict()
+                if print_counter == PRINT_TRIGGER:
                     print_counter = 0
                     print(cur, ' work in progress...')
-        commit(cursor, model_path)
-        cursor = dict()
+        input_file.close()
+        commit(current_data, model_path)
+        current_data = dict()
         if cur != sys.stdin:
             print("Trained " + cur)
     print("Train successful")
@@ -105,21 +120,22 @@ def train(model_path, input_paths, is_lower):
     compress(model_path)
     print("Compressed successful")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--model", action="store",
-                    help="model file path", required="true")
-parser.add_argument("-i", "--input_dir", action="store",
-                    help="input directory path")
-parser.add_argument("--lc", action="store_true",
-                    help="all text concidered lower case")
 
-args = parser.parse_args(input().split())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", action="store",
+                        help="model file path", required=True)
+    parser.add_argument("-i", "--input_dir", action="store",
+                        help="input directory path")
+    parser.add_argument("--lc", action="store_true",
+                        help="all text considered lower case")
 
-if args.input_dir is None:
-    train(args.model, sys.stdin, args.lc)
-else:
-    files = os.listdir(args.input_dir)
-    files = list(map(lambda x:os.path.join(args.input_dir, x), files))
-    txt = filter(lambda x: x.endswith('.txt'), files)
-    train(args.model, txt, args.lc)
-    
+    args = parser.parse_args(input().split())
+
+    if args.input_dir is None:
+        train(args.model, sys.stdin, args.lc)
+    else:
+        files = os.listdir(args.input_dir)
+        files = list(map(lambda x: os.path.join(args.input_dir, x), files))
+        txt = filter(lambda x: x.endswith('.txt'), files)
+        train(args.model, txt, args.lc)
