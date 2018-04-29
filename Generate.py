9761e2541a1f30cf9dcbf0dca300320088b755e2
Fyodor Bukreev <Fyodor.bukreev@yandex.ru>
2018-04-29 19:41:43 +0300

    Fixed Generate.py codestyle

--- Generate.py
+++ Generate.py
@@ -21,39 +21,53 @@ if __name__ == "__main__":
 
     if args.output is not None:
         out = open(args.output, 'w')
+
+    # start of text generation
     cur_word = args.seed
     counter = int(args.length)
     while counter > 1:
         if cur_word is None:
-                word_list = os.listdir(args.model)
-                cur_word = numpy.random.choice(word_list)
-                cur_word = cur_word[0:-2]
-                if args.output is not None:
-                    out.write('\n')
-                else:
-                    print()
+            # generate random word
+            word_list = os.listdir(args.model)
+            cur_word = numpy.random.choice(word_list)
+            cur_word = cur_word[0:-2]
+            if args.output is not None:
+                out.write('\n')
+            else:
+                print()
+
+        # printing another word
         counter -= 1
         if args.output is not None:
             out.write(cur_word + " ")
         else:
             print(cur_word, end=' ')
+
+        # opening file for word written
         word_file_name = os.path.join(args.model, cur_word) + ".w"
+
         if os.path.exists(word_file_name) and os.path.isfile(word_file_name):
+            # reading file
             word_file = open(word_file_name, 'r')
             sm = 0
             poss_word = list()
             possibility = list()
-            for I in word_file:
-                tmp = I.split()
+
+            # initializing poss_word and possibility
+            for word_number in word_file:
+                tmp = word_number.split()
                 poss_word.append(tmp[0])
                 possibility.append(tmp[1])
                 sm += int(tmp[1])
 
+            # normalization of possibility
             for i in range(len(possibility)):
                 possibility[i] = int(possibility[i]) / sm
 
+            # choosing next word
             cur_word = numpy.random.choice(poss_word, p=possibility)
         else:
             cur_word = None
+
     if args.output is not None:
         out.close()
