c1f508c31ca83b4e6f9e23dd70d9ed436de5312d
Fyodor Bukreev <Fyodor.bukreev@yandex.ru>
2018-05-06 03:59:32 +0300

    Generate.py codestyle improvements

--- Generate.py
+++ Generate.py
@@ -3,7 +3,43 @@ import numpy
 import os
 
 
-if __name__ == "__main__":
+# this function generate random word from all in model
+def generate_random_word(model):
+    # model is a path to model, given by user
+    return numpy.random.choice(os.listdir(model))[0:-2]
+
+
+# this function generate random word from .w file given
+def choose_next_word(word_file_name):
+    # word_file_name is a path to .w file related to the word
+    # we are generating successor for
+
+    if os.path.exists(word_file_name) and os.path.isfile(word_file_name):
+        # reading file
+        word_file = open(word_file_name, 'r')
+        sm = 0
+        poss_word = list()
+        possibility = list()
+
+        # initializing poss_word and possibility
+        for word_number in word_file:
+            tmp = word_number.split()
+            poss_word.append(tmp[0])
+            possibility.append(tmp[1])
+            sm += int(tmp[1])
+
+        # normalization of possibility
+        for i in range(len(possibility)):
+            possibility[i] = int(possibility[i]) / sm
+
+        # choosing next word
+        return numpy.random.choice(poss_word, p=possibility)
+    else:
+        return None
+
+
+# This function initialize parser
+def init_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model", action="store",
                         help="model file path", required=True)
@@ -13,7 +49,11 @@ if __name__ == "__main__":
                         help="lengs of text in words")
     parser.add_argument("-o", "--output", action="store",
                         help="output file path")
+    return parser
 
+
+if __name__ == "__main__":
+    parser = init_parser()
     args = parser.parse_args(input().split())
 
     if not os.path.exists(args.model) or os.path.isfile(args.model):
@@ -28,9 +68,8 @@ if __name__ == "__main__":
     while counter > 1:
         if cur_word is None:
             # generate random word
-            word_list = os.listdir(args.model)
-            cur_word = numpy.random.choice(word_list)
-            cur_word = cur_word[0:-2]
+            cur_word = generate_random_word(args.model)
+
             if args.output is not None:
                 out.write('\n')
             else:
@@ -43,31 +82,7 @@ if __name__ == "__main__":
         else:
             print(cur_word, end=' ')
 
-        # opening file for word written
-        word_file_name = os.path.join(args.model, cur_word) + ".w"
-
-        if os.path.exists(word_file_name) and os.path.isfile(word_file_name):
-            # reading file
-            word_file = open(word_file_name, 'r')
-            sm = 0
-            poss_word = list()
-            possibility = list()
-
-            # initializing poss_word and possibility
-            for word_number in word_file:
-                tmp = word_number.split()
-                poss_word.append(tmp[0])
-                possibility.append(tmp[1])
-                sm += int(tmp[1])
-
-            # normalization of possibility
-            for i in range(len(possibility)):
-                possibility[i] = int(possibility[i]) / sm
-
-            # choosing next word
-            cur_word = numpy.random.choice(poss_word, p=possibility)
-        else:
-            cur_word = None
+        cur_word = choose_next_word(os.path.join(args.model, cur_word) + ".w")
 
     if args.output is not None:
         out.close()
